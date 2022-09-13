fb82bee02c4a491de5e8e47259cd4e8841321f12
Gavriel Elizedek <gavriel@certora.com>
2022-09-13 13:19:02 +0300

    change package nae to CVLDoc , adding entry poiny to CVLDoc.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 NAME = "CVLDoc"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 if __name__ == "__main__":
     setuptools.setup(
@@ -15,9 +15,10 @@ if __name__ == "__main__":
         description=" Utility for reading smart contracts files, parse and export their NatSpec comments to JSON files.",
         long_description=long_description,
         long_description_content_type="text/markdown",
+        keywords='Certora, CVLDoc',
         url="https://github.com/Certora/natspecTools",
-        packages=setuptools.find_packages(),
-        package_dir={'CVLDoc': 'src/natspecTools'},
+        packages=setuptools.find_packages(where='src'),
+        package_dir={'': 'src'},
         include_package_data=True,
         data_files=[('examples', ['tests/Test/definition_test.spec',
                                   'tests/Test/import_test.spec',
@@ -29,6 +30,7 @@ if __name__ == "__main__":
                                   'tests/Test/methods_test.spec',
                                   'tests/Test/using_test.spec'])],
         install_requires=[
+            'cffi',
             'natspec_parser==0.2.1'
         ],
         classifiers=[
@@ -38,4 +40,9 @@ if __name__ == "__main__":
             "Development Status :: 3 - Alpha",
         ],
         python_requires='>=3.5',
+        entry_points={
+            "console_scripts": [
+                "cvlDoc=natspecTools.natspec_to_json:entry_point",
+            ],
+        },
     )
